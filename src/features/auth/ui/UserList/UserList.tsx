0304cda76a69cc14f039aafd9f2fb10566ee5b0e
Anton.Khilmanovich <anton.khilmanovich@life.com.by>
2024-07-26 15:18:25 +0300

    feat: add supabase, auth

---
+++ src/features/auth/ui/UserList/UserList.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@mantine/core';
+import { supabaseClient } from '@shared/api/supabase/supabaseClient.ts';
+import { useEffect, useState } from 'react';
+
+export const UserList = () => {
+  const [isSigOunLoading, setIsSigOunLoading] = useState(false);
+
+  const handleLogout = async () => {
+    try {
+      setIsSigOunLoading(true);
+
+      await supabaseClient.auth.signOut();
+    } finally {
+      setIsSigOunLoading(false);
+    }
+  };
+
+  // const { t } = useTranslation();
+  //
+  const getUsers = async () => {
+    const { data } = await supabaseClient.from('users').select();
+
+    console.log('response', data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <Button loading={isSigOunLoading} onClick={handleLogout}>
+        logout
+      </Button>
+    </div>
+  );
+};
