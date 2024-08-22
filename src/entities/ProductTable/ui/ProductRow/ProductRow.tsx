048bfd2aceef9425888410c631f46c40ff990a66
maksim_obodovskii <maksim469@mail.ru>
2024-08-22 16:06:28 +0300

    feat: added translations, useHandlesForProduct, useForm hooks, Custom Modal, ProductTableModalForm, errorsHandler

--- src/entities/ProductTable/ui/ProductRow/ProductRow.tsx
+++ src/entities/ProductTable/ui/ProductRow/ProductRow.tsx
@@ -1,7 +1,8 @@
 import { formatDate } from '@/shared/lib/const/formatDate';
-import { Table } from '@mantine/core';
+import { Table, Image } from '@mantine/core';
 
 import { IProduct } from '../../model/types/IProduct';
+import classes from './ProductRow.module.css';
 
 interface ProductRowProps {
   product: IProduct;
@@ -12,9 +13,16 @@ export const ProductRow = (props: ProductRowProps): JSX.Element => {
 
   return (
     <Table.Tr key={product.id}>
-      <Table.Td>{product.id}</Table.Td>
-      <Table.Td>{formatDate(product.created_at)}</Table.Td>
-      <Table.Td>{product.category_id}</Table.Td>
+      <Table.Td className={classes.cellTd}>{product.id}</Table.Td>
+      <Table.Td className={classes.cellTd}>{formatDate(product.created_at)}</Table.Td>
+      <Table.Td className={classes.cellTd}>{product.category_id}</Table.Td>
+      <Table.Td className={classes.cellTd}>{product.name}</Table.Td>
+      <Table.Td className={classes.cellTd}>{product.composition}</Table.Td>
+      <Table.Td className={classes.cellTd}>{`${product.weight} г.`}</Table.Td>
+      <Table.Td className={classes.cellTd}>{`${product.price} руб.`}</Table.Td>
+      <Table.Td className={classes.cellTd}>
+        <Image radius="md" h={50} w="auto" fit="cover" src={product.image} />
+      </Table.Td>
     </Table.Tr>
   );
 };
